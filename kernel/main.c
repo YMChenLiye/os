ab7035a2f2f0f6b208be836a0800eb47bfde8822
ymchenliye <ymchenliye@gmail.com>
2016-04-16 19:18:17 +0800

    增加打印数字功能

--- kernel/main.c
+++ kernel/main.c
@@ -2,6 +2,19 @@
 
 int main(void){
 	put_str("i am a kernel!\n");
+	put_int(0);
+	put_char('\n');
+	put_int(9);
+	put_char('\n');
+	put_int(0x12345678);
+	put_char('\n');
+	put_int(12345678);
+	put_char('\n');
+	put_int(0x00000000);
+	put_char('\n');
+	put_char(65);
+	put_char('\n');
+	put_int(41);
 	while(1) ;
 }
 
