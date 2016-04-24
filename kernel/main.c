e01eace14bf27db359efed8d38d1de4439a30c11
ymchenliye <ymchenliye@gmail.com>
2016-04-24 14:00:15 +0800

    do some test

--- kernel/main.c
+++ kernel/main.c
@@ -12,13 +12,13 @@ int main(void){
 	thread_start("k_thread_a",31,k_thread_a,"argA");
 
 	while(1) ;
+	return 0;
 }
 
 void k_thread_a(void* arg)
 {
 	char* para = arg;
-	while(1){
-		put_str(para);
-	}
+	put_str(para);
+	return ;
 }
 
