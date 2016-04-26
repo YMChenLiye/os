4141ca56bd5681f3f0c6cee52dabec87dd4e1d6b
ymchenliye <ymchenliye@gmail.com>
2016-04-26 19:27:26 +0800

    完成键盘驱动

--- kernel/init.c
+++ kernel/init.c
@@ -5,6 +5,7 @@
 #include "memory.h"
 #include "thread.h"
 #include "console.h"
+#include "keyboard.h"
 
 //负责初始化所有模块
 void init_all(){
@@ -14,4 +15,5 @@ void init_all(){
 	thread_init();
 	timer_init();	//初始化PIT
 	console_init();	//控制台初始化最好放在开中断之前
+	keyboard_init();	//键盘初始化
 }
