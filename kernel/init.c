6e9b1f374196ff4e22756f62e0215c42b27ed206
ymchenliye <ymchenliye@gmail.com>
2016-04-27 22:57:22 +0800

    定义并初始化TSS

--- kernel/init.c
+++ kernel/init.c
@@ -6,6 +6,7 @@
 #include "thread.h"
 #include "console.h"
 #include "keyboard.h"
+#include "tss.h"
 
 //负责初始化所有模块
 void init_all(){
@@ -16,4 +17,5 @@ void init_all(){
 	timer_init();	//初始化PIT
 	console_init();	//控制台初始化最好放在开中断之前
 	keyboard_init();	//键盘初始化
+	tss_init();			//tss初始化
 }
