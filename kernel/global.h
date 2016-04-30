ebddb88ab3d87c41bcbbc20b7341e10dba4aa864
ymchenliye <ymchenliye@gmail.com>
2016-04-30 22:54:35 +0800

    实现系统调用

--- kernel/global.h
+++ kernel/global.h
@@ -72,7 +72,7 @@ struct gdt_desc {
 #define		IDT_DESC_32_TYPE	0xE			//32位的门
 #define 	IDT_DESC_16_TYPE	0x6			//16位的门，不用，定义它只是和32位门区分
 #define		IDT_DESC_ATTR_DPL0	((IDT_DESC_P<<7) + (IDT_DESC_DPL0<<5) + IDT_DESC_32_TYPE)
-#define		IDT_DESC_ARRT_DPL3	((IDT_DESC_P<<7) + (IDT_DESC_DPL3<<5) + IDT_DESC_32_TYPE)
+#define		IDT_DESC_ATTR_DPL3	((IDT_DESC_P<<7) + (IDT_DESC_DPL3<<5) + IDT_DESC_32_TYPE)
 
 
 
--- thread/thread.c
+++ thread/thread.c
@@ -7,12 +7,14 @@
 #include "print.h"
 #include "memory.h"
 #include "process.h"
+#include "sync.h"
 
 #define PG_SIZE 4096
 
 struct task_struct* main_thread;		//主线程PCB
 struct list thread_ready_list;			//就绪队列
 struct list thread_all_list;			//所有任务队列
+struct lock pid_lock;					//分配pid锁
 static struct list_elem* thread_tag;	//用于保存队列中的线程结点
 
 extern void switch_to(struct task_struct* cur,struct task_struct* next);
@@ -30,6 +32,16 @@ static void kernel_thread(thread_func function,void* func_arg){
 	function(func_arg);
 }
 
+
+//分配pid
+static pid_t allocate_pid(void){
+	static pid_t next_pid = 0;
+	lock_acquire(&pid_lock);
+	next_pid++;
+	lock_release(&pid_lock);
+	return next_pid;
+}
+
 //初始化线程栈thread_stack，将待执行的函数和参数放到thread_stack中相应的位置
 void thread_create(struct task_struct* pthread,thread_func function,void* func_arg){
 	//先预留中断使用的栈的空间，可见thread.h中的定义的结构
@@ -44,6 +56,7 @@ void thread_create(struct task_struct* pthread,thread_func function,void* func_a
 //初始化线程基本信息
 void init_thread(struct task_struct* pthread,char* name,int prio){
 	memset(pthread,0,sizeof(*pthread));
+	pthread->pid = allocate_pid();
 	strcpy(pthread->name,name);
 
 	if(pthread == main_thread){
@@ -156,6 +169,7 @@ void thread_init(void){
 	put_str("thread_init start\n");
 	list_init(&thread_ready_list);
 	list_init(&thread_all_list);
+	lock_init(&pid_lock);
 	//将当前main函数创建为线程
 	make_main_thread();
 	put_str("thread_init done\n");
