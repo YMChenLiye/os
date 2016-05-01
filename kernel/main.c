c0acc1e179f01053b4c84643932c8ee0c052e539
ymchenliye <ymchenliye@gmail.com>
2016-05-01 15:11:00 +0800

    实现printf

--- kernel/main.c
+++ kernel/main.c
@@ -6,6 +6,7 @@
 #include "process.h"
 #include "syscall-init.h"
 #include "syscall.h"
+#include "stdio.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
@@ -20,10 +21,10 @@ int main(void) {
    process_execute(u_prog_a, "user_prog_a");
    process_execute(u_prog_b, "user_prog_b");
 
-   intr_enable();
    console_put_str(" main_pid:0x");
    console_put_int(sys_getpid());
    console_put_char('\n');
+   intr_enable();
    thread_start("k_thread_a", 31, k_thread_a, "argA ");
    thread_start("k_thread_b", 31, k_thread_b, "argB ");
 
@@ -37,9 +38,6 @@ void k_thread_a(void* arg) {
    console_put_str(" thread_a_pid:0x");
    console_put_int(sys_getpid());
    console_put_char('\n');
-   console_put_str(" prog_a_pid:0x");
-   console_put_int(prog_a_pid);
-   console_put_char('\n');
    while(1);
 }
 
@@ -49,20 +47,19 @@ void k_thread_b(void* arg) {
    console_put_str(" thread_b_pid:0x");
    console_put_int(sys_getpid());
    console_put_char('\n');
-   console_put_str(" prog_b_pid:0x");
-   console_put_int(prog_b_pid);
-   console_put_char('\n');
    while(1);
 }
 
 /* 测试用户进程 */
 void u_prog_a(void) {
-   prog_a_pid = getpid();
+	char* name = "prog_a";
+   printf(" I am %s,my pid : %d%c",name,getpid(),'\n');
    while(1);
 }
 
 /* 测试用户进程 */
 void u_prog_b(void) {
-   prog_b_pid = getpid();
+	char* name = "prog_b";
+   printf(" I am %s,my pid : %d%c",name,getpid(),'\n');
    while(1);
 }
--- lib/user/syscall.h
+++ lib/user/syscall.h
@@ -3,9 +3,11 @@
 #include "stdint.h"
 
 enum SYSCALL_NR{
-	SYS_GETPID
+	SYS_GETPID,
+	SYS_WRITE
 };
 
 uint32_t getpid(void);
+uint32_t write(char* str);
 
 #endif
