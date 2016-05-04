c895b58dedec6112086e4d3968879b6f825556aa
ymchenliye <ymchenliye@gmail.com>
2016-05-04 15:58:22 +0800

    fix

--- thread/thread.c
+++ thread/thread.c
@@ -21,7 +21,7 @@ static struct list_elem* thread_tag;	//用于保存队列中的线程结点
 extern void switch_to(struct task_struct* cur,struct task_struct* next);
 
 //系统空闲时运行的线程
-static void idle(void* arg){
+static void idle(void* arg UNUSED){
 	while(1){
 		thread_block(TASK_BLOCKED);
 		//执行hlt时必须保证目前处在开中断的情况下
