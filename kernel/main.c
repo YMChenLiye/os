dbf7f0232a50025dbd636aaa12dbf8c890005b72
ymchenliye <ymchenliye@gmail.com>
2016-05-10 13:51:33 +0800

    实现创建目录

--- kernel/main.c
+++ kernel/main.c
@@ -24,7 +24,7 @@ int main(void) {
 //	process_execute(u_prog_b, "u_prog_b");
 //	thread_start("k_thread_a", 31, k_thread_a, "argA ");
 //	thread_start("k_thread_b", 31, k_thread_b, "argB ");
-/*	uint32_t fd = sys_open("/file1",O_RDWR);
+/*	uint32_t fd = sys_open("/file1",O_RDWR | O_CREAT);
 	printf("open /file1,fd:%d\n",fd);
 	char buf[64] = {0};
 	int read_bytes = sys_read(fd,buf,18);
@@ -46,7 +46,25 @@ int main(void) {
 
 	sys_close(fd);
 */
-	printf("/file1 delete %s!\n",sys_unlink("/file1") == 0 ? "done" : "fail");
+//	printf("/file1 delete %s!\n",sys_unlink("/file1") == 0 ? "done" : "fail");
+
+//	int fd1 = sys_open("/file1",O_RDWR | O_CREAT);
+//	sys_write(fd1,"hello,world\n",12);
+	
+	printf("/dir1/subdir1 create %s!\n",sys_mkdir("/dir1/subdir1") == 0 ? "done" : "fail");
+	printf("/dir1 create %s!\n",sys_mkdir("/dir1") == 0 ? "done" : "fail");
+	printf("now, /dir1/subdir1 create %s!\n",sys_mkdir("/dir1/subdir1") == 0 ? "done" : "fail");
+	int fd = sys_open("/dir1/subdir1/file2",O_CREAT|O_RDWR);
+	if(fd != -1){
+		printf("dir1/subdir1/file2 create done!\n");
+		sys_write(fd,"Catch me if you can!\n",21);
+		sys_lseek(fd,0,SEEK_SET);
+		char buf[32] = {0};
+		sys_read(fd,buf,21);
+		printf("/dir1/subdir1/file2 says:\n%s",buf);
+		sys_close(fd);
+	}
+
 
 	while(1);
 	return 0;
